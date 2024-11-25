793002461047810a8219aad52796db2d7211bd8d
juanjemdIos <juanjemd@gmail.com>
2024-11-25 16:03:18 +0100

    Upated scripts. Improvement related to issue 130
    It has been necessary to update the queries and functions to align with the points in issue 130 and the suggestions from colleagues.

--- ttl_transformations.py
+++ ttl_transformations.py
@@ -1,7 +1,7 @@
 from rdflib import Graph
 import pystache
 import os
-
+import logging
 # query sparql para obtneer los valores del test
 
 query = """
@@ -36,19 +36,18 @@ WHERE {
 }
 """
 
-
 query_metrics = """
 PREFIX dcterms: <http://purl.org/dc/terms/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX dcat: <http://www.w3.org/ns/dcat#> 
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
 PREFIX ftr: <https://www.w3id.org/ftr#>
-PREFIX dqv: <http://www.w3.org/ns/dqv#> 
+PREFIX dqv: <http://www.w3.org/ns/dqv#>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX doap: <http://usefulinc.com/ns/doap#>
-PREFIX dqv: <http://www.w3.org/ns/dqv#> 
-PREFIX dav: <https://example.org/ontology#> 
+PREFIX dqv: <http://www.w3.org/ns/dqv#>
+PREFIX dav: <https://example.org/ontology#>
 
-SELECT DISTINCT ?s ?title ?label ?description ?keywords ?version ?indimension ?label_dimension ?desc_indimension
+SELECT DISTINCT ?s ?title ?label ?description ?keywords ?version ?license ?indimension ?label_dimension ?desc_indimension
 ?publisher ?test ?creator_name ?creator_orcid ?landing_page ?benchmark ?bm_title ?bm_desc ?metric_status
 WHERE {
     ?s a dqv:Metric .
@@ -58,10 +57,11 @@ WHERE {
     ?s dcterms:publisher ?publisher .
     ?s dcat:keyword ?keywords .
     ?s dcat:version ?version .
+    ?s dcterms:license ?license .
     ?s dcat:landingPage ?landing_page .
     ?s dqv:inDimension ?indimension .
     ?s ftr:metricStatus ?metric_status .
-    ?s ftr:hasBenchmark ?benchmark . 
+    ?s ftr:hasBenchmark ?benchmark .
     ?indimension rdfs:label ?label_dimension .
     ?indimension dcterms:description ?desc_indimension .
     ?benchmark a dav:MetricBenchmark ;
@@ -72,6 +72,38 @@ WHERE {
     ?creator_orcid vcard:fn ?creator_name .
 }
 """
+
+query_benchmarks = """
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX ftr: <https://www.w3id.org/ftr#>
+PREFIX dqv: <http://www.w3.org/ns/dqv#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX doap: <http://usefulinc.com/ns/doap#>
+PREFIX dqv: <http://www.w3.org/ns/dqv#>
+PREFIX dav: <https://example.org/ontology#>
+
+SELECT DISTINCT ?s ?title ?label ?description ?keywords ?version ?license
+ ?creator_name ?creator_orcid ?landing_page ?benchmark_status ?associatedMetric ?metricIdentifier ?metricLabel
+WHERE {
+    ?s a dav:MetricBenchmark .
+    ?s dcterms:title ?title .
+    ?s rdfs:label ?label .
+    ?s dcterms:description ?description .
+    ?s dcat:keyword ?keywords .
+    ?s dcat:version ?version .
+    ?s dcterms:license ?license .
+    ?s dcat:landingPage ?landing_page .
+    ?s ftr:status ?benchmark_status .
+    ?s dcterms:creator ?creator_orcid .
+    ?creator_orcid vcard:fn ?creator_name .
+    ?s ftr:associatedMetric ?associatedMetric .
+    ?associatedMetric dcterms:identifier ?metricIdentifier .
+    ?associatedMetric rdfs:label ?metricLabel .
+}
+"""
+# ?metric dqv:Metric .
 
 
 def ttl_to_html(path_ttl, path_mustache, query):
@@ -95,7 +127,8 @@ def ttl_to_html(path_ttl, path_mustache, query):
         'test_publisher': '',
         'test_metric': '',
         'test_repository': '',
-        'test_creators': ''
+        'test_creators': '',
+        'test_turtle': ''
     }
 
     # como hay varias keywords normalemnte, las meto en un array y luego las uno en un string separadas por comas.
@@ -118,7 +151,10 @@ def ttl_to_html(path_ttl, path_mustache, query):
         data['test_publisher'] = row.publisher
         data['test_metric'] = row.metric
         data['test_repository'] = row.web_repository
-        keywords.append(str(row.keywords))
+        data['test_turtle'] = row.label + '.ttl'
+
+        if str(row.keywords) not in keywords:
+            keywords.append(str(row.keywords))
 
         if str(row.creator_name) not in creators:
             creators.append(str(row.creator_name))
@@ -174,6 +210,100 @@ def ttl_to_jsonld(path_ttl):
     print(f'Archivo creado: {path_jsonld}')
 
 
+def ttl_to_html_benchmarks(path_ttl, path_mustache, query):
+    g = Graph()
+    g.parse(path_ttl, format="turtle")
+    # Ejecutar la consulta
+    results = g.query(query)
+
+    data = {
+        'benchmark_identifier': '',
+        'benchmark_title': '',
+        'benchmark_name': '',
+        'benchmark_description': '',
+        'benchmark_keywords': '',
+        'benchmark_version': '',
+        'benchmark_license': '',
+        'benchmark_creators': '',
+        'benchmark_landing_page': '',
+        'benchmark_metrics': '',
+        'benchmark_status': '',
+        'benchmark_turtle': ''
+    }
+
+    # como hay varias keywords normalemnte, las meto en un array y luego las uno en un string separadas por comas.
+    keywords = []
+
+    # lo mismo ocurre con los creadores que son dos
+    creators = []
+    creators_orcid = []
+
+    metrics = []
+    metrics_uri = []
+
+    for row in results:
+
+        data['benchmark_identifier'] = row.s
+        data['benchmark_title'] = row.title
+        data['benchmark_name'] = row.label
+        data['benchmark_description'] = row.description
+        data['benchmark_version'] = row.version
+        data['benchmark_license'] = row.license
+        data['benchmark_landing_page'] = row.landing_page
+        data['benchmark_status'] = row.benchmark_status
+        data['benchmark_turtle'] = row.label.replace('Benchmark ', '') + '.ttl'
+
+        if str(row.keywords) not in keywords:
+            keywords.append(str(row.keywords))
+
+        if str(row.creator_name) not in creators:
+            creators.append(str(row.creator_name))
+
+        if str(row.creator_orcid) not in creators_orcid:
+            creators_orcid.append(str(row.creator_orcid))
+
+        if str(row.metricIdentifier) not in metrics_uri:
+            metrics_uri.append(str(row.metricIdentifier))
+
+        if str(row.metricLabel) not in metrics:
+            metrics.append(str(row.metricLabel))
+
+        all_keywords = ", ".join(keywords)
+
+        result = []
+        for nombre, orcid in zip(creators, creators_orcid):
+            result.append(f'<a href="{orcid}" target="_blank">{nombre}</a>')
+
+        resultMetrics = []
+        for nameMetric, uriMetric in zip(metrics, metrics_uri):
+            resultMetrics.append(
+                f'<a href="{uriMetric}" target="_blank">{nameMetric}</a>')
+
+        all_creators = ', '.join(result)
+        all_metrics = ', '.join(resultMetrics)
+
+    data['benchmark_keywords'] = all_keywords
+
+    data['benchmark_creators'] = all_creators
+
+    data['benchmark_metrics'] = all_metrics
+    # Cargar la plantilla mustache
+    with open(path_mustache, 'r') as template_file:
+        template_content = template_file.read()
+
+    # sustituir la plantilla con los datos del diccionario
+    renderer = pystache.Renderer()
+    rendered_output = renderer.render(template_content, data)
+
+    # guardamos el html. El path es el mismo que el ttl pero cambiando la extension
+    path_html = os.path.splitext(path_ttl)[0] + '.html'
+
+    with open(path_html, 'w') as output_file:
+        output_file.write(rendered_output)
+
+    print(f'Archivo creado: {path_html}')
+
+
 def ttl_to_html_metrics(path_ttl, path_mustache, query):
 
     g = Graph()
@@ -188,6 +318,7 @@ def ttl_to_html_metrics(path_ttl, path_mustache, query):
         'metric_description': '',
         'metric_keywords': '',
         'metric_version': '',
+        'metric_license': '',
         'metric_uri_inDimension': '',
         'metric_inDimension': '',
         'metric_desc_dimension': '',
@@ -198,7 +329,8 @@ def ttl_to_html_metrics(path_ttl, path_mustache, query):
         'metric_benchmark': '',
         'metric_benchmark_title': '',
         'metric_benchmark_desc': '',
-        'metric_status': ''
+        'metric_status': '',
+        'metric_turtle': ''
     }
 
     # como hay varias keywords normalemnte, las meto en un array y luego las uno en un string separadas por comas.
@@ -214,6 +346,7 @@ def ttl_to_html_metrics(path_ttl, path_mustache, query):
         data['metric_name'] = row.label
         data['metric_description'] = row.description
         data['metric_version'] = row.version
+        data['metric_license'] = row.license
         data['metric_uri_inDimension'] = row.indimension
         data['metric_inDimension'] = row.label_dimension
         data['metric_desc_dimension'] = row.desc_indimension
@@ -224,7 +357,10 @@ def ttl_to_html_metrics(path_ttl, path_mustache, query):
         data['metric_benchmark_title'] = row.bm_title
         data['metric_benchmark_desc'] = row.bm_desc
         data['metric_status'] = row.metric_status
-        keywords.append(str(row.keywords))
+        data['metric_turtle'] = row.label.replace('Metric ', '') + '.ttl'
+
+        if str(row.keywords) not in keywords:
+            keywords.append(str(row.keywords))
 
         if str(row.creator_name) not in creators:
             creators.append(str(row.creator_name))
@@ -261,28 +397,51 @@ def ttl_to_html_metrics(path_ttl, path_mustache, query):
     print(f'Archivo creado: {path_html}')
 
 
-def iterate_paths(path, template, query, isTest):
+def iterate_paths(path, template, query, typeDoc):
+
+    # param typeDoc
+    # T : test
+    # M : metric
+    # B : benchmark
 
     for root, dirs, files in os.walk(path):
+        if root == path:
+            continue
+
         for file in files:
             if file.endswith(".ttl"):
                 # si encontramos el archivo ttl podemos llamar a las funciones de transformacion
                 path_ttl = os.path.join(root, file)
-                print(path_ttl)
-                if isTest:
-                    ttl_to_html(path_ttl, template, query)
-                else:
-                    ttl_to_html_metrics(path_ttl, template, query)
+                match typeDoc:
+                    case "T":
+                        ttl_to_html(path_ttl, template, query)
+                    case "M":
+                        ttl_to_html_metrics(path_ttl, template, query)
+                    case "B":
+                        ttl_to_html_benchmarks(path_ttl, template, query)
+                    case _:
+                        print("Unknown type doc")
 
                 ttl_to_jsonld(path_ttl)
 
 
 # github paths
+path_ttls_benchmarks = 'https://github.com/oeg-upm/fair_ontologies/tree/main/doc/benchmark/'
 path_ttls_metrics = 'https://github.com/oeg-upm/fair_ontologies/tree/main/doc/metric/'
 path_ttls = 'https://github.com/oeg-upm/fair_ontologies/tree/main/doc/test/'
 path_mustache = 'https://github.com/oeg-upm/fair_ontologies/tree/main/doc/test/template.html'
 path_mustache_metrics = 'https://github.com/oeg-upm/fair_ontologies/tree/main/doc/metric/template_metrics.html'
+path_mustache_benchmarks = 'https://github.com/oeg-upm/fair_ontologies/tree/main/doc/benchmark/template_benchmark.html'
+
+# path_ttls_benchmarks = '/Users/mbp_jjm/Documents/DOCUMENTACION UPM/Fair_Ontologies/doc/benchmark/'
+# path_ttls_metrics = '/Users/mbp_jjm/Documents/DOCUMENTACION UPM/Fair_Ontologies/doc/metric/'
+# path_ttls = '/Users/mbp_jjm/Documents/DOCUMENTACION UPM/Fair_Ontologies/doc/test/'
+# path_mustache = '/Users/mbp_jjm/Documents/DOCUMENTACION UPM/Fair_Ontologies/doc/test/template.html'
+# path_mustache_metrics = '/Users/mbp_jjm/Documents/DOCUMENTACION UPM/Fair_Ontologies/doc/metric/template_metrics.html'
+# path_mustache_benchmarks = '/Users/mbp_jjm/Documents/DOCUMENTACION UPM/Fair_Ontologies/doc/benchmark/template_benchmark.html'
 
 
-iterate_paths(path_ttls_metrics, path_mustache_metrics, query_metrics, False)
-iterate_paths(path_ttls, path_mustache, query, True)
+iterate_paths(path_ttls_metrics, path_mustache_metrics, query_metrics, 'M')
+iterate_paths(path_ttls, path_mustache, query, 'T')
+iterate_paths(path_ttls_benchmarks, path_mustache_benchmarks,
+              query_benchmarks, 'B')
